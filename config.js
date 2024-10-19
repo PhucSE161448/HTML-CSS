1ba1bfacef0976fbf9a6ce57beb85b5301142cf8
Phúcc <112921620+PhucSE161448@users.noreply.github.com>
2024-10-19 22:27:54 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Em Yêu';
+const giftUrl = 'https://www.facebook.com/mphuc1012/';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng Em';
+const contentCard = 'Chúc bé iu của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const giftImage = '2.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
